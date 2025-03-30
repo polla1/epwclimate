f4b802699871cbb97babc8332bdb1a82d0fee164
polla1 <polakoyi@yahoo.com>
2025-03-30 17:28:35 +0300

    Create contact.py

---
+++ contact.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def display_contact():
+    with st.sidebar:
+        st.header("Contact")
+        st.markdown("""
+        **GitHub:** [polla1](https://github.com/polla1)  
+        **Issues:** Please report any issues through GitHub
+        """)
