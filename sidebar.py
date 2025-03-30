ba962604d2314cdcaa0f6967e0630009c882158c
polla1 <polakoyi@yahoo.com>
2025-03-30 17:28:08 +0300

    Create sidebar.py

---
+++ sidebar.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def display_sidebar():
+    with st.sidebar:
+        st.header("Custom Data Upload")
+        uploaded = st.file_uploader(
+            "Upload EPW files",
+            type="epw",
+            accept_multiple_files=True
+        )
+    return uploaded
